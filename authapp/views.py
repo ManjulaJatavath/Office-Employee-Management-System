ea55e4fcd3f2df73c0ef00a7e2367abd805131fa
Manjula Jatavath <144055077+ManjulaJatavath@users.noreply.github.com>
2024-04-25 14:09:40 +0530

    views.py

--- authapp/views.py
+++ authapp/views.py
@@ -26,16 +26,6 @@ def signup(request):
         return redirect('/')
     
     return render(request, 'signup.html')
-# def signup(request):
-#     if request.method == 'POST':
-#         form = UserCreationForm(request.POST)
-#         if form.is_valid():
-#             form.save()
-#             return redirect('login')
-#     else:
-#         form = UserCreationForm()
-#     return render(request, 'signup.html', {'form': form})
-
 
 def login_view(request):
     if request.method == 'POST':
