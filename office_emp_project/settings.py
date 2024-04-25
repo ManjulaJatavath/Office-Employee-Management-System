e875d9c50d3fc6e61f64134f2717b379ec7c7a22
ManjulaJatavath <manjulajatavath554@gmail.com>
2024-04-25 15:16:48 +0530

    code updated

--- office_emp_project/settings.py
+++ office_emp_project/settings.py
@@ -121,10 +121,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS=[
-    os.path.join(BASE_DIR,'static')
-]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 django_heroku.settings(locals())
 
 # Default primary key field type
